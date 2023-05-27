c324e44ac7d3440c945c66be0460c023f3a70fa4
pgd <rudeh1253@gmail.com>
2023-05-27 13:30:26 +0900

    feat: put login form on main screen

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
+    <Login/>
   );
 }
 
