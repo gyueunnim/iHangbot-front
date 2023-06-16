358d9375d2448471c65ac02e3af0739971ffe377
pgd <rudeh1253@gmail.com>
2023-06-16 11:10:49 +0900

    add: formStyles.js and design layout login

---
+++ formStyles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const formStyles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    inputContainer: {
+        width: "100%",
+        justifyContent: "flex-start",
+    },
+    label: {
+
+    },
+    input: {
+    },
+    btnContainer: {
+        width: "100%",
+        justifyContent: "flex-start",
+    },
+    btnLogin: {
+        alignItems: "center",
+        justifyContent: "center",
+    }
+});
+
+export default formStyles;
