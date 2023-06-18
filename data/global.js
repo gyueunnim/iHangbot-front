58f901a22852d7c6f62922d55d65db845535bcd6
pgd <rudeh1253@gmail.com>
2023-06-18 14:47:46 +0900

    add: file for storing global constants

---
+++ data/global.js
@@ -0,0 +1,5 @@
+export const serverUrl = "http://192.168.0.177:8080/";
+export const loginUrl = "http://192.168.0.177:8080/member/login";
+export const signUpUrl = "http://192.168.0.177:8080/member/signUp";
+export const getProfileUrl = (userId) => `http://192.168.0.177:8080/member/${userId}"/profile`;
+export const chatbotUrl = "http://192.168.0.204:8079/api/chat";
