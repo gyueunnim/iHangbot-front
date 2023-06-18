93947bd31156a4d3f2795330bf25056ea405dac5
pgd <rudeh1253@gmail.com>
2023-06-18 17:09:28 +0900

    add: list rest api urls

--- data/global.js
+++ data/global.js
@@ -1,5 +1,18 @@
-export const serverUrl = "http://192.168.0.177:8080/";
-export const loginUrl = "http://192.168.0.177:8080/member/login";
-export const signUpUrl = "http://192.168.0.177:8080/member/signUp";
-export const getProfileUrl = (userId) => `http://192.168.0.177:8080/member/${userId}"/profile`;
+export const restServerUrl = "http://192.168.0.177:8080";
+
+// keyword
+export const urlPostReport = restServerUrl + "/keyword/report";
+export const urlPostGetKeyword = restServerUrl + "/keyword/getKeyWord";
+export const urlPostGetConcern = restServerUrl + "/keyword/getConcern";
+
+// sentiment
+export const urlPostSentimentReport = restServerUrl + "/sentiment/report";
+export const urlPostGetSentimentData = restServerUrl + "/sentiment/getData";
+
+// member
+export const urlPutProfile = (memberId) => restServerUrl + `/member/${memberId}/profile/setting`;
+export const urlPostSignUp = restServerUrl + "/member/signUp";
+export const urlGetLogin = restServerUrl + "/member/login";
+export const urlGetProfile = (memberId) => restServerUrl + `/member/${memberId}"/profile`;
+
 export const chatbotUrl = "http://192.168.0.204:8079/api/chat";
